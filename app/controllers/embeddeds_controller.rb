ba3d053d87de9fb685b38e2c7bbc38f985c4e629
Jen Young <jyoung488@gmail.com>
2017-06-02 11:46:32 -0700

    Set up MVC for Embedded Signature Requests

---
+++ app/controllers/embeddeds_controller.rb
@@ -0,0 +1,6 @@
+class EmbeddedsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+  end
+end
---
+++ app/models/embedded.rb
@@ -0,0 +1,3 @@
+class Embedded < ApplicationRecord
+
+end
