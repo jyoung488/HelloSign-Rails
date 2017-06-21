664b17e4b822a91894a8aa35ad2be26574c39ce1
Jen Young <jyoung488@gmail.com>
2017-06-21 09:12:13 -0700

    Hide key

--- config/initializers/hello_sign.rb
+++ config/initializers/hello_sign.rb
@@ -1,6 +1,6 @@
 require 'hello_sign'
 HelloSign.configure do |config|
-  config.api_key = '0f09c444c7b8b650844cfb4ceb387191a02b9e3ca241c757574d72448080ebd6'
+  config.api_key = ENV['HS_KEY']
   # You can use email_address and password instead of api_key. But api_key is recommended
   # If api_key, email_address and password are all present, api_key will be used
   # config.email_address = 'email_address'
